e9bc43d97c8547231e831c562cc0458f856679fa
akanshuchauhan <akanshuchauhan2000@gmail.com>
2023-12-12 18:13:38 +0530

    first commit

---
+++ 01/index.js
@@ -0,0 +1,17 @@
+
+require('dotenv').config()
+console.log(process.env)
+const express = require('express')
+const app = express()
+ 
+
+ 
+
+const port = process.env.PORT||4000
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
